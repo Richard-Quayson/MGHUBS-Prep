be3a1f02197cb6a66f0d595ba3afb68d0afb3426
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-02-24 19:42:59 +0000

    configured routes for account and crud apps

--- backend_assignment/todo/todo/urls.py
+++ backend_assignment/todo/todo/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('crud.urls'), name='crud-api'),
+    path('api/account/', include('account.urls'), name='account-api'),
+    path('api/todo/', include('crud.urls'), name='crud-api'),
 ]
