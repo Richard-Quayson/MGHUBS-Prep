f0bfe9a7afe7d71d5d8f06214e547d30993d51e1
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-02-24 19:46:26 +0000

    configured routes for crud endpoints

--- backend_assignment/todo/crud/urls.py
+++ backend_assignment/todo/crud/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import AccountRegistrationView
+from .views import (
+    AddToDoView, ToDoListView, RetrieveToDoView, 
+    MarkToDoAsCompletedView, UpdateToDoView, DeleteToDoView
+)
 
 urlpatterns = [
-    path('register/', AccountRegistrationView.as_view(), name='register'),
+    path("add/", AddToDoView.as_view(), name="add"),
+    path("get/", ToDoListView.as_view(), name="list"),
+    path("get/<int:todo_id>/", RetrieveToDoView.as_view(), name="retrieve"),
+    path("update/<int:todo_id>/", UpdateToDoView.as_view(), name="update"),
+    path("complete/<int:todo_id>/", MarkToDoAsCompletedView.as_view(), name="complete"),
+    path("delete/<int:todo_id>/", DeleteToDoView.as_view(), name="delete"),
 ]
